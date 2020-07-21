a750c7a80f0afa4d35965d9a7a077de9a8fe92bb
depjoti <depjotighosh@gmail.com>
2020-07-22 02:16:24 +0600

    Update projects

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -4,7 +4,7 @@ const projects = [
     title: "React-Searching-Image",
     img: "searching_image.PNG",
     description: "Using Unsplash API to fetch data to show images with react",
-    type: ["../../images/icons/react.svg"],
+    type: ["../../images/icons/js.svg", "../../images/icons/react.svg"],
     github: "https://github.com/depjoti/React-Searching-Image",
     demo: "https://epic-wozniak-e60eeb.netlify.app/",
   },
@@ -21,6 +21,15 @@ const projects = [
     ],
     github: "https://github.com/depjoti/Contact-Keeper",
     demo: "https://protected-reaches-24881.herokuapp.com/login",
+  },
+  {
+    id: 19,
+    title: "Seasons",
+    img: "season.PNG",
+    description: "Using Geolocation API to detect Seasons",
+    type: ["../../images/icons/js.svg", "../../images/icons/react.svg"],
+    github: "https://github.com/depjoti/seasons",
+    demo: "https://sleepy-meitner-0343c8.netlify.app/",
   },
 ];
 
